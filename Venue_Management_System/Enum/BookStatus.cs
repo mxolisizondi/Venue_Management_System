28d9803c9da5ded4eb947d5b5ce201b659485610
Mxolisi <mxolisizondi20@gmail.com>
2021-05-03 22:49:17 +0200

    Revert "Merge branch 'master' into new_changes"
    This reverts commit 6958557c90ca497583ffeb29b44b541eb32a4ffd, reversing
    changes made to e955edb4bac7158cf2ca1373f674804947b37541.

---
+++ Venue_Management_System/Enum/BookStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Venue_Management_System.Enum
+{
+    public class BookStatus
+    {
+        public const byte Active = 1;
+        public const byte InActive = 2;
+    }
+}
